dd1dc39a03fe0bfe0ba5c5ca475228004456f4b3
Varulli <120419874+Varulli@users.noreply.github.com>
2024-07-03 19:36:30 -0400

    Add error messages to login form

--- routes/login.js
+++ routes/login.js
@@ -4,8 +4,20 @@ const passport = require("passport");
 
 /* GET login page. */
 router.get("/", function (req, res, next) {
+  let error;
+  if (req.session.messages) {
+    error = req.session.messages[req.session.messages.length - 1];
+    req.session.messages.length = 0;
+  }
+
   if (res.locals.user) res.redirect("/");
-  else res.render("login", { title: "Login" });
+  else
+    res.render("login", {
+      title: "Login",
+      username: req.body.username,
+      password: req.body.password,
+      error,
+    });
 });
 
 /* POST login page. */
