5a6f8c92868e92e184e1d4b01927f69748c28d4e
Varulli <120419874+Varulli@users.noreply.github.com>
2024-07-03 18:52:48 -0400

    Add validation to sign-up form

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { checkSchema, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const User = require("../models/user");
@@ -12,7 +13,68 @@ router.get("/", function (req, res, next) {
 /* POST sign-up page. */
 router.post(
   "/",
+  checkSchema({
+    username: {
+      in: ["body"],
+      trim: true,
+      escape: true,
+      isLength: {
+        errorMessage: "Username must be 3-20 characters long.",
+        options: { min: 3, max: 20 },
+      },
+      custom: {
+        options: async (value) => {
+          const user = await User.findOne({ username: value }).exec();
+          return user
+            ? Promise.reject("Username is already in use.")
+            : Promise.resolve();
+        },
+      },
+    },
+    password: {
+      in: ["body"],
+      trim: true,
+      escape: true,
+      isLength: {
+        errorMessage: "Password must be 8-20 characters long.",
+        options: { min: 8, max: 20 },
+      },
+      custom: {
+        options: (value) => {
+          if (!value.match(/[a-z]/))
+            throw new Error("Password must include a lowercase letter.");
+          if (!value.match(/[A-Z]/))
+            throw new Error("Password must include an uppercase letter.");
+          if (!value.match(/\d/))
+            throw new Error("Password must include a number.");
+          return true;
+        },
+      },
+    },
+    confirm_password: {
+      in: ["body"],
+      trim: true,
+      escape: true,
+      custom: {
+        options: (value, { req }) => {
+          if (value === req.body.password) return true;
+          else throw new Error("Passwords do not match.");
+        },
+      },
+    },
+  }),
   async function (req, res, next) {
+    // Re-render the sign-up page with error messages if there are any
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("sign-up", {
+        title: "Sign Up",
+        username: req.body.username,
+        password: req.body.password,
+        errors: new Map(errors.array().map((error) => [error.path, error.msg])),
+      });
+    }
+
     bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
       if (err) return next(err);
       try {
@@ -30,6 +92,7 @@ router.post(
       }
     });
   },
+  // Log in with the new user
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
