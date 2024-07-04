bf7e75ba7d56e293b8ac20337f7fa84f8662546f
Varulli <120419874+Varulli@users.noreply.github.com>
2024-07-04 16:57:31 -0400

    Create model for boards

---
+++ models/board.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const luxon = require("luxon");
+
+const Board = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  createdAt: { type: Date, default: Date.now() },
+});
+
+Board.virtual("createdAtFormatted").get(function () {
+  return luxon.DateTime.fromJSDate(this.createdAt).toLocaleString(
+    luxon.DateTime.DATETIME_MED
+  );
+});
+
+module.exports = mongoose.model("Board", Board);
