e92dba76a6895153af28fbb8e7ece992ff59839d
Varulli <120419874+Varulli@users.noreply.github.com>
2024-07-05 18:47:03 -0400

    Create board page

--- routes/board.js
+++ routes/board.js
@@ -4,7 +4,12 @@ const Board = require("../models/board");
 
 /* GET board page. */
 router.get("/:id", async function (req, res, next) {
-  const board = await Board.findById(req.params.id).populate("user").exec();
+  const board = await Board.findById(req.params.id)
+    .populate("user", "username")
+    .populate("posts")
+    .exec();
+  for (const post of board.posts) post.populate("user", "username");
+  console.log(board);
 
   res.render("board", {
     title: board.title,
